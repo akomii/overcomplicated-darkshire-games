67a57a6a59a75e289c2651a8af6e99635c78c94c
akom <alexkombie@gmail.com>
2023-04-13 13:28:06 +0200

    fixed empty history from json request

--- src/main/java/org/example/potato/model/EventHistory.java
+++ src/main/java/org/example/potato/model/EventHistory.java
@@ -3,12 +3,19 @@ package org.example.potato.model;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.LinkedList;
 import java.util.List;
 
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
 @Component
 @Scope("application")
 @JacksonXmlRootElement(localName = "eventHistory")
@@ -16,7 +23,7 @@ public class EventHistory {
     
     @JacksonXmlProperty(localName = "event")
     @JacksonXmlElementWrapper(useWrapping = false)
-    private final List<Event> history = new LinkedList<>();
+    final List<Event> history = new LinkedList<>();
     
     public void add(Event event) {
         history.add(event);
