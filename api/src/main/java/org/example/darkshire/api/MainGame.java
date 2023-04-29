2a28b96a43b829b0ec2ba6e17c5d91a6789bd3cb
akom <alexkombie@gmail.com>
2023-04-30 01:15:21 +0200

    moved MainGame out of models

--- api/src/main/java/org/example/darkshire/api/MainGame.java
+++ api/src/main/java/org/example/darkshire/api/MainGame.java
@@ -1,6 +1,8 @@
-package org.example.darkshire.api.model;
+package org.example.darkshire.api;
 
 import org.example.darkshire.api.enums.GameAttribute;
+import org.example.darkshire.api.model.EventHistory;
+import org.example.darkshire.api.model.GameState;
 
 public interface MainGame<T extends Enum<T> & GameAttribute> {
     
--- api/src/main/java/org/example/darkshire/api/base/AbstractMainGame.java
+++ api/src/main/java/org/example/darkshire/api/base/AbstractMainGame.java
@@ -5,15 +5,15 @@ import org.example.darkshire.api.enums.GameAttribute;
 import org.example.darkshire.api.model.Event;
 import org.example.darkshire.api.model.EventHistory;
 import org.example.darkshire.api.model.GameState;
-import org.example.darkshire.api.model.MainGame;
+import org.example.darkshire.api.MainGame;
 
-abstract class AbstractMainGame<T extends Enum<T> & GameAttribute> implements MainGame<T> {
+public abstract class AbstractMainGame<T extends Enum<T> & GameAttribute> implements MainGame<T> {
     
     @Getter
-    GameState<T> gameState;
+    protected GameState<T> gameState;
     
     @Getter
-    EventHistory<T> eventHistory;
+    protected EventHistory<T> eventHistory;
     
     protected void executeEvent(Event<T> event) {
         gameState.consume(event);
