ecb165362a4eeb38e8352e6b52320355f0c60b74
akom <alexkombie@gmail.com>
2023-04-29 22:58:22 +0200

    moved implementation of EventHistory

--- common/src/main/java/org/example/darkshire/common/model/EventHistory.java
+++ common/src/main/java/org/example/darkshire/common/model/EventHistory.java
@@ -1,4 +1,4 @@
-package org.example.potato.model;
+package org.example.darkshire.common.model;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
@@ -7,7 +7,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.context.annotation.Scope;
+import org.example.darkshire.api.model.Event;
 import org.springframework.stereotype.Component;
 
 import java.util.LinkedList;
@@ -15,14 +15,13 @@ import java.util.List;
 
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@Getter
 @Component
-@Scope("application")
 @JacksonXmlRootElement(localName = "eventHistory")
 public class EventHistory {
     
     @JacksonXmlProperty(localName = "event")
     @JacksonXmlElementWrapper(useWrapping = false)
+    @Getter
     final List<Event> history = new LinkedList<>();
     
     public void add(Event event) {
